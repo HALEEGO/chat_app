6e34310b5033e3ab6e18936538e66c586286feda
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2021-07-15 14:34:57 +0900

    feat: make router

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import Stomp from 'stompjs';
-import SockJS from 'sockjs-client';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Main from './app/components/main';
+import Talk from './app/components/talk';
 
-// const { Stomp } = StompJs;
-// client.onConnect = (frame) => {
-//   let client = Stomp.over(() => new WebSocket('/gs-guide-websocket'));
-// };
-let stompClient: Stomp.Client;
-
-const App = () => {
-  const [chatMessages, setChatMessages] = useState(['hello', 'nice']);
-  const [message, setMessage] = useState('');
-  const [isConnect, setConnect] = useState(false);
-
-  function onConnect() {
-    const sockJS = new SockJS('http://localhost:8080/chat');
-    stompClient = Stomp.over(sockJS);
-    setConnect(true);
-  }
-
-  function sendName() {
-    stompClient.send('/app/hello', {}, message);
-  }
-
-  useEffect(() => {
-    if (!isConnect) {
-      return;
-    }
-    stompClient.connect({}, () => {
-      stompClient.subscribe('/topic/greetings', (greeting) => {
-        const newMessage = greeting.body;
-        setChatMessages((prev) => [...prev, newMessage]);
-      });
-    });
-  }, [chatMessages, isConnect]);
-
-  return (
-    <div>
-      <ul>
-        <li>
-          <button
-            type="button"
-            onClick={() => {
-              onConnect();
-            }}
-          >
-            connect
-          </button>
-        </li>
-        <li>
-          <button type="button" onClick={() => {}}>
-            disconnect
-          </button>
-        </li>
-      </ul>
-      <div>
-        <input type="text" placeholder="message" value={message} onChange={(e) => setMessage(e.target.value)} />
-        <button
-          type="button"
-          onClick={() => {
-            sendName();
-            setMessage('');
-          }}
-        >
-          send
-        </button>
-      </div>
-      <div>출력화면</div>
-      <ul>
-        {chatMessages.map((result, i) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <li key={i}>{result}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact render={() => <Main />} />
+      <Route path="/talk" exact component={Talk} />
+    </Switch>
+  </Router>
+);
 
 export default App;
---
+++ src/app/components/talk.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Talk() {
+  return <div>talk</div>;
+}
+
+export default Talk;
