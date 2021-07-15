703c48825fa4e4fe39d06a7a0fc28004b2ac1423
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2021-07-15 17:19:51 +0900

    bug: props send error

--- src/app/components/talk.tsx
+++ src/app/components/talk.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// import { useLocation } from 'react-router';
+import Stomp from 'stompjs';
+import SockJS from 'sockjs-client';
 
-function Talk() {
-  return <div>talk</div>;
-}
+const sockJS = new SockJS('http://localhost:8080/chat');
+const stompClient = Stomp.over(sockJS);
+
+const Talk = (props: any) => {
+  // const [chatMessages, setChatMessages] = useState(['hello', 'nice']);
+  const [message, setMessage] = useState('');
+  // const location = useLocation();
+  // const name = location.state;
+  console.log(props);
+  useEffect(() => {
+    stompClient.connect({}, () => {
+      stompClient.subscribe('/topic/greetings', (greeting) => {
+        const newMessage = greeting.body;
+        setMessage(newMessage);
+        setMessage(message);
+      });
+    });
+    // stompClient.send('/app/hello', {}, );
+  });
+
+  return <div>씨발 왜안댐{props.location.state}</div>;
+};
 
 export default Talk;
